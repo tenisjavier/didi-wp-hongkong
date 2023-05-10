7895a726e2c1e4063b63b00525f48078a1fb1610
tenisjavier <tenis.javier@gmail.com>
2023-05-10 11:49:26 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `didi-wp-hongkong`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
